b39f53cd7ac2b12154737e60532ca429f46a0b10
Bilal Layache <bilal.layache@gmail.com>
2024-07-11 10:17:37 +0200

    Joc de daus

---
+++ Joc de Daus/S05T02N0layachebilal/src/main/java/cat/itacademy/barcelonactiva/layache/bilal/s05/t02/S05T02N0layachebilalApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.layache.bilal.s05.t02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T02N0layachebilalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T02N0layachebilalApplication.class, args);
+	}
+
+}
